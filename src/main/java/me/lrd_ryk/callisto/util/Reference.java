ae0e5557f6eaa1d55cbf4967cfa1490937d68866
YuccaPlantMan <79335188+YuccaPlantMan@users.noreply.github.com>
2021-03-06 12:47:16 -0600

    Add files via upload
    had to re-upload changes after deleting callistoPostman.

--- src/main/java/me/lrd_ryk/callisto/util/Reference.java
+++ src/main/java/me/lrd_ryk/callisto/util/Reference.java
@@ -3,7 +3,7 @@ package me.lrd_ryk.callisto.util;
 public class Reference {
 	
 	public static final String MOD_ID = "Extcerlity";
-	public static final String NAME = "callistoPostman";
+	public static final String NAME = "Callisto";
 	public static final String VERSION = "0.0.1";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "";
