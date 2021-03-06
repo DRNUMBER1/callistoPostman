eaaf1b2b48a58cff3d0e8a8626ca6d566d8b8aaf
YuccaPlantMan <79335188+YuccaPlantMan@users.noreply.github.com>
2021-03-06 12:32:51 -0600

    Add files via upload

---
+++ src/main/java/me/lrd_ryk/callisto/util/Reference.java
@@ -0,0 +1,12 @@
+package me.lrd_ryk.callisto.util;
+
+public class Reference {
+	
+	public static final String MOD_ID = "Extcerlity";
+	public static final String NAME = "callistoPostman";
+	public static final String VERSION = "0.0.1";
+	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "";
+	public static final String COMMON_PROXY_CLASS = "";
+
+}
