9be6815ca33014b54fc8eaa57af6f8b5d6281adf
Rykun <66620681+DRNUMBER1@users.noreply.github.com>
2021-03-07 11:18:59 +0530

    Just some more coode PEWWWWWW

--- src/main/java/me/lrd_ryk/callisto/Main.java
+++ src/main/java/me/lrd_ryk/callisto/Main.java
@@ -1,5 +1,31 @@
 package me.lrd_ryk.callisto;
 
-public class Main {
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import me.lrd_ryk.callisto.util.Reference;
 
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION) 
+public class Main {
+	
+	@Instance
+	public Main instance;
+	
+	@EventHandler
+	public void PreInit (FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void init (FMLPreInitializationEvent event) {
+		MinecraftForge.EVENT_BUS.register(instance);
+	}
+		
+	
+	@EventHandler
+	public void PostInit (FMLPreInitializationEvent event) {
+	
+ }
 }
